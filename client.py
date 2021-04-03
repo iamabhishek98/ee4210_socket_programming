1754194a987fda8efe5334310ce16f993b613494
Abhishek <saiabhishek.karnati@u.nus.edu>
2021-04-03 18:24:34 +0800

    client threading

--- client.py
+++ client.py
@@ -1,11 +1,20 @@
 import socket
+import threading
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = socket.gethostname()  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+def thread_fn(id):
+    for i in range(1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        s.sendall(b'Hello, world')
+        data = s.recv(1024)
+        print(id,'Received', repr(data))
 
-print('Received', repr(data))
+x = threading.Thread(target=thread_fn,args=(1,))
+# y = threading.Thread(target=thread_fn,args=(2,))
+# z = threading.Thread(target=thread_fn,args=(3,))
+x.start()
+# y.start()
+# z.start()
