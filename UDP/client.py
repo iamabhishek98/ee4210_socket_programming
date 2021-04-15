5bbad5252c6f41e5a73426382d8694bfb17dca80
Abhishek <saiabhishek.karnati@u.nus.edu>
2021-04-15 17:59:27 +0800

    added fork to UDP server

--- UDP/client.py
+++ UDP/client.py
@@ -7,7 +7,8 @@ PORT = 12345
 
 def execute(id):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.sendto("Request".encode('utf-8'),(HOST,PORT))
+    req = b'GET / HTTP/1.1\r\nHost: 192.168.56.1:65432\r\nConnection: keep-alive\r\nDNT: 1\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n'
+    s.sendto(req,(HOST,PORT))
     data, addr = s.recvfrom(1024)
     print ('Data Received by Client '+str(id)+':',str(data))
     s.close()
--- UDP/server.py
+++ UDP/server.py
@@ -1,4 +1,4 @@
-import socket, sys
+import socket, os, sys
 
 try:
     HOST = socket.gethostname()
@@ -7,6 +7,22 @@ except:
     sys.exit()
 
 PORT = 12345
+
+def createHTTPResponse(addr,body):
+    HTTPResponse = ("HTTP/1.1 200 OK\r\n"
+                + "Host: {}:{}\r\n".format(addr[0],addr[1])
+                + "Accept-Ranges: bytes\r\n"
+                + "Content-Length: {}\r\n".format(len(body))
+                + "Content-Type: text/html\r\n"
+                + "\r\n" + body)
+    return bytes(HTTPResponse, 'utf-8')
+
+def onNewClent(conn, addr):
+    data = conn.decode('utf8')
+    if "GET / HTTP/1.1" in data:
+        body = "<html><body>EE-4210:Continuous assessment</body></html>\n"
+        s.sendto(createHTTPResponse(addr,body),addr)
+        print('Sent webpage to {}:{}'.format(addr[0],addr[1]))
 
 if __name__ == '__main__':
     try:
@@ -17,12 +33,10 @@ if __name__ == '__main__':
     
     s.bind((HOST, PORT))
     print('Socket binded to '+str(socket.gethostbyname(HOST))+':'+str(PORT))
-
+    
     while True:
         conn, addr = s.recvfrom(1024)
-        s.sendto(
-            b"HTTP/1.1 200 OK\n"
-            + b"Content-Type: text/html\n"
-            + b"\n"
-            + b"<html><body>EE-4210:Continuous assessment</body></html>\n",addr)
-        print('Webpage sent to', addr)
+        pid = os.fork()
+        if pid == 0:
+            onNewClent(conn, addr)
+            sys.exit()
